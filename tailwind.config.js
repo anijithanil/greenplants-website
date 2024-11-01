745bc318dfbb15e771ee953d9c46b4272a5bcdd7
Anijith <anijith7056@gmail.com>
2024-11-01 11:45:26 +0530

    Green plants web started

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        pacifico: ["Pacifico", "serif"], // Corrected fallback font
+      },
+    },
+  },
+  plugins: [],
+};
